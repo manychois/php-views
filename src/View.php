28380594ad728e2c126255975576e0de28d78ff0
Siu Pang Tommy Choi <manychois@gmail.com>
2023-06-18 16:46:12 +1000

    Add View back for backward compatibility

---
+++ src/View.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Manychois\Views;
+
+/**
+ * Base class for building view template.
+ *
+ * @deprecated Use AbstractView instead.
+ */
+abstract class View extends AbstractView
+{
+}
